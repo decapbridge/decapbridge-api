1d2d67741f5aa658cc1704f3aca46630e320fba1
Alexandre Lotte <dev@alexlotte.ca>
2024-06-26 11:25:44 -0400

    signup notification fix

--- src/signup-notification/index.ts
+++ src/signup-notification/index.ts
@@ -3,14 +3,15 @@ import type { MailService, SettingsService } from '@directus/api/services/index'
 
 const hook: HookConfig = async ({ action }, ctx) => {
   const adminEmail = ctx.env['ADMIN_EMAIL'];
+  const publicUrl = String(ctx.env['PUBLIC_URL']);
   if (adminEmail) {
     const schema = await ctx.getSchema();
     const mail = new (ctx.services.MailService as typeof MailService)({ schema });
     const settings = new (ctx.services.SettingsService as typeof SettingsService)({ schema });
     const { project_name } = await settings.readSingleton({});
-    action('users.create', async ({ payload }) => {
+    action('users.create', async ({ payload }, { accountability }) => {
       ctx.logger.info(`User ${payload.email} just signed up.`);
-      if (payload.email !== adminEmail) {
+      if (payload.email !== adminEmail && !publicUrl.includes('localhost') && !accountability?.user) {
         await mail.send({
           to: adminEmail,
           subject: `A new user just signed up to ${project_name}`,
